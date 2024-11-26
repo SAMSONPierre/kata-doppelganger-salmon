fdae52e72cb10928de42335d41f6fd8ac6035c6c
Gilbert Ziade <gilbert_z2001@hotmail.com>
2024-11-26 14:57:31 +0100

    [pushing the calculator

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
+    SafeCalculator calculator = new SafeCalculator(() -> true);
+
+    int result = calculator.add(1, 2);
+
+    assert(result == 3);
     // TODO: write a test to demonstrate the bug in SafeCalculator.add()
   }
 }
